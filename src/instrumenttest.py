1b4f6500d14c919d51c55833fe97006aecb9a276
Vijay Panchal <vijayrameshpanchal@gmail.com>
2023-09-14 07:56:11 +0530

    morning

--- src/instrumenttest.py
+++ src/instrumenttest.py
@@ -4,39 +4,50 @@ import sys
 from termcolor import cprint
 
 
-cprint("----------------------------Instrument check------------------------\n\n\n","yellow")
-
-try:                            # try for RS232/serial connection
-    ser = serial.Serial()
-    ser = serial.Serial('COM1', baudrate=9600, timeout=1)
-    cprint("serial connection at COM1","green")
-    cprint("WANT TO PROCEED (y)","blue")
-    if input()=="y":
-        pass
-    else:
-        sys.exit()
-    connection = "RS232"
-except:
-    cprint("WARNING: no RS-232 connection detected","blue",attrs=['bold'])
 
 
-try:                            # try for GPIB connection
-    resource = pyvisa.ResourceManager()
-    print(resource.list_resources())
-    SR830 = resource.open_resource(resource.list_resources()[0]) 
-    cprint(SR830,"green")
-    if input()=="y":
-        pass
-    else:
-        sys.exit()
-    connection = "GPIB"
+def instrumenttest():
+
+    cprint("----------------------------Instrument check------------------------\n\n\n","yellow")
     
-except:
-    cprint("WARNING: no GPIB connection detected","blue",attrs=['bold'])
+    try:                            # try for RS232/serial connection
+        ser = serial.Serial()
+        ser = serial.Serial('COM1', baudrate=9600, timeout=1)
+        cprint("serial connection at COM1","green")
+        cprint("WANT TO PROCEED (y)","blue")
+        if input()=="y":
+            pass
+        else:
+            sys.exit()
+        connection = "RS232"
+    except:
+        cprint("WARNING: no RS-232 connection detected","blue",attrs=['bold'])
 
 
-if connection == "none":
-    cprint("ERROR: No connection detected.","red",attrs=['bold'])
-    sys.exit()
+    try:                            # try for GPIB connection
+        resource = pyvisa.ResourceManager()
+        print(resource.list_resources())
+        SR830 = resource.open_resource(resource.list_resources()[0]) 
+        cprint(SR830,"green")
+        if input()=="y":
+            pass
+        else:
+            sys.exit()
+        connection = "GPIB"
+    
+    except:
+        cprint("WARNING: no GPIB connection detected","blue",attrs=['bold'])
 
-cprint("----------------------------Instrument test finished------------------------\n\n\n","yellow")
+
+    if connection == "none":
+        cprint("ERROR: No connection detected.","red",attrs=['bold'])
+        sys.exit()
+
+    cprint("----------------------------Instrument test finished------------------------\n\n\n","yellow")
+
+
+
+
+
+if __name__=="__main__":
+    print("tests are not written") 
---
+++ src/main.py
@@ -0,0 +1,10 @@
+import dfile
+import instrumenttest
+import time
+
+# this is main files where loops gone take part
+
+
+def readdata(dev):
+    time.sleep(0.01)
+    dev.write("")
