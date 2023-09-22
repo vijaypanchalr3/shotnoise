2ba56f5af1f81163fc8652760483c9c43d03090b
vijay panchal <vijay@debian.deb>
2023-09-22 10:51:29 +0530

    final dump from machine 1

--- src/experiment.py
+++ src/experiment.py
@@ -1,30 +1,16 @@
 from pyinstro import SR830
+from pyinstro import coloroutput
 
 
-import time
+# get device from pyinstro
+device = SR830()
 
 
-new_instance = SR830()
+# get device ready
+device.reset()
 
-new_instance.ping()
-time.sleep(2)
-new_instance.reset()
-time.sleep(2)
 
-while True:
-    if input('n'):
-        break
-    try:
-        freq = float(input())
-        new_instance.set_frequency(freq)
-        for i in range(10):
-            time.sleep(2)
-            data = new_instance.get_data_explicitly(data_variable=3)
-            new_instance.writerow(data)
-            print(str(i)+")  ", "data taken: ", str(data))
-    except:
-        print("give proper frequency")
-    time.sleep(2)
-    
-SR830.close()
-exit()
+
+
+
+
