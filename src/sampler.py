0b6123053cf7aec09ea41460c19e47ada8347de1
Vijay Panchal <vijayrameshpanchal@gmail.com>
2023-09-30 21:10:38 +0530

    new data added

--- src/sampler.py
+++ src/sampler.py
@@ -25,25 +25,22 @@ class sampler:
 
     def discrete_range(self,minimum,maximum,step):
         # time.sleep(2)
-        count = 1
         
         self.device.set_frequency(minimum)
-        
-        self.device.auto_gain()
+        input()
+        self.device.autogain()
         for freq in range(minimum,maximum+1,step):
             self.device.set_frequency(freq)
             print(freq)
-            time.sleep(.5)
+            self.device.autogain()
+            time.sleep(.8)
+            
             for i in range(100):
+                # print(self.device.get_data_explicitly(3))
                 data= float(self.device.get_data_explicitly(3))
                 self.device.longwriterow([freq,data])
                 # print(freq,data)
-                time.sleep(0.1)
-            if count>=100:
-                self.device.auto_gain()
-                count=1
-            else:
-                count+=step
+                time.sleep(0.02)
     def partition_loop(self,minimum, maximum,partitions,timedelay=0.2):
         # time.sleep(2)
         frange = numpy.linspace(minimum, maximum,partitions)
@@ -67,6 +64,5 @@ class sampler:
 
 if __name__=="__main__":
     x = sampler()
-    x.discrete_range(500,10000,10)
-    # x.discrete_range(25000,60000,50)
+    x.discrete_range(300,4000,2)
     sys.exit()
